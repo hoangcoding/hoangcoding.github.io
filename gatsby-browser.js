206543af663ee91aa69968ec0b7c291ccb0603c9
Hoang Mai <hoangcoding@gmail.com>
2019-12-16 15:23:50 -0800

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import wrapPageElementWithLayout from './src/helpers/wrapPageElement'
-
-// Global styles
-import './src/styles/global.scss'
-
-// Layout with Page Transitions
-export const wrapPageElement = wrapPageElementWithLayout
--- src/helpers/wrapPageElement.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-import { Layout } from "../components/organisms/Layout"
-
-const wrapPageElement = ({ element, props }: { element: any, props: any }) => (
-  <Layout {...props}>{element}</Layout>
-)
-export default wrapPageElement
